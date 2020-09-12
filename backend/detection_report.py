2e60c2da37a16e09d4358f4a568c7ad673bc3e14
edi <edi@fleetonomy.io>
2020-09-12 20:58:03 +0300

    Backend - Save to DynamoDB, Device - Don't upload blurred images

--- backend/detection_report.py
+++ backend/detection_report.py
@@ -1,34 +1,59 @@
 import json
 import os
-import random
 from datetime import datetime as dt
 
 import boto3
 
 
 def lambda_handler(event, context):
-    # TODO - Save to DynamoDB and get the PK
     print(f"Received {event}")
-    report_id = random.randint(0, 1000)
 
-    presigned_image_url, s3_bucket, s3_image_path = create_upload_url(report_id)
+    # Save record in DynamoDB
+    record = create_record(event)
+    save_dynamo_db(record)
+
+    presigned_image_url, s3_bucket, s3_image_path = create_upload_url(record['s3_filepath'])
     publish_topic(f"to/device/{event['client_id']}", {
-        'report_id': report_id,
+        'month': record['month'],
+        'report_time': record['report_time'],
         'upload_url': presigned_image_url,
         'bucket': s3_bucket,
         'image_path': s3_image_path
     })
 
 
-def create_upload_url(report_id):
+def create_record(event):
+    now = dt.now()
+    report_time = f"{now.day}_{now.hour}_{now.minute}_{now.second}_{now.microsecond}"
+    s3_filename = f"{report_time}.jpg"
+    s3_filepath = f"{dt.now().year}/{dt.now().month}/{s3_filename}"
+
+    record = {
+        **event,
+        'month': f"{now.year}_{now.month}",  # DynamoDB Partition Key
+        'report_time': report_time,  # DynamoDB Sort Key
+        's3_filename': s3_filename,
+        's3_filepath': s3_filepath
+    }
+
+    return record
+
+
+def save_dynamo_db(record):
+    dynamodb = boto3.resource('dynamodb')
+    table = dynamodb.Table('detections')
+    response = table.put_item(Item=record)
+
+    return response
+
+
+def create_upload_url(s3_image_path):
     s3_client = boto3.client('s3')
 
     # AWS Bucket
     s3_bucket = os.environ['BUCKET_NAME']
 
     # Generate presigned product image urls:
-    s3_image_path = f"{dt.now().year}/{dt.now().month}/{dt.now().day}/{dt.now().hour}/{report_id}.jpg"
-
     presigned_image_url = s3_client.generate_presigned_post(
         Bucket=s3_bucket,
         Key=s3_image_path,
