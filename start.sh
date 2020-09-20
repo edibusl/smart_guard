e3213cd6b54cb5e6bee77cf15992f23e778de885
edi <edi@fleetonomy.io>
2020-09-20 21:10:54 +0300

    Crontab start script

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
-source /home/pi/opencv_course/venv/bin/activate
-cd /home/pi/smartguard
-export PYTHONPATH="."
-export PYTHONUNBUFFERED=1
-python device/start.py > smartguard.log 2> smartguard.log &
-echo "exit code: "
-echo $?
+# intended to be started through crontab on raspberry pi:
+# * * * * * cd /home/pi/smartguard && bash start.sh > start.log
+
+if pgrep -x "python" > /dev/null
+then
+    echo "Smartguard is already  running. Skipping."
+else
+    source /home/pi/opencv_course/venv/bin/activate
+    cd /home/pi/smartguard
+    export PYTHONPATH="."
+    export PYTHONUNBUFFERED=1
+    python device/start.py > smartguard.log 2> smartguard.log &
+    echo "Started smartguard. Exit code: $?"
+fi
